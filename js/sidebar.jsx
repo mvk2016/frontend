c8cb845f0bcf0537423017de77fe0d8ac925b10c
Erik Dackebro <erik.dackebro@gmail.com>
2016-02-24 13:36:35 +0100

    Updated sidebar with styleing

--- js/sidebar.jsx
+++ js/sidebar.jsx
@@ -50,14 +50,25 @@ class SidebarComponent extends React.Component {
     return this.state.visible ? (
       <div id='sidebar'>
         <span onClick={this.handleClose} className='glyphicon glyphicon-remove close'></span>
-        <h1>{this.state.current.roomname}</h1>
-        <div id='data'>
-          <h2>Data</h2>
-          <ul>
+        <br />
+        <br />
+
+        <h1 className="sidebar_title">{this.state.current.roomname}</h1>
+        <br />
+
+        <div id='data' className="sidebar_item">
+          <div className="sidebar_item_head">
+            <h2 className="sidebar_title">Data</h2>
+          </div>
+          <div className="sidebar_subitem_contaier">
             {this.state.current.data.map(item => (
-              <li key={item.name}>{item.name}: {item.value} <i>{item.lastupdate}</i></li>
+                <div className="sidebar_subitem">
+                  <p className="sidebar_subitem_text left_align_stretch">{item.name}</p>
+                  <p className="sidebar_subitem_text right_align_stretch">{item.value}</p>
+                </div>
+              //<li key={item.name}>{item.name}: {item.value} <i>{item.lastupdate}</i></li>
             ))}
-          </ul>
+          </div>
         </div>
         <LineChart data={this.state.history} />
       </div>
