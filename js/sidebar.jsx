651cbb63bf18cb7d229d99ff1bb4f94c7fb6dee7
Andreas Mårtensson <addem1234@gmail.com>
2016-02-24 14:33:23 +0100

    Style changes

--- js/sidebar.jsx
+++ js/sidebar.jsx
@@ -47,26 +47,28 @@ class SidebarComponent extends React.Component {
   }
 
   render() {
+    var data = this.state.current.data;
     return this.state.visible ? (
       <div id='sidebar'>
         <span onClick={this.handleClose} className='glyphicon glyphicon-remove close'></span>
         <br />
         <br />
 
-        <h1 className="sidebar_title">{this.state.current.roomname}</h1>
+        <h1>{this.state.current.roomname}</h1>
         <br />
 
         <div id='data' className="sidebar_item">
           <div className="sidebar_item_head">
-            <h2 className="sidebar_title">Data</h2>
+            <h2>Data</h2>
           </div>
           <div className="sidebar_subitem_contaier">
-            {this.state.current.data.map(item => (
-                <div className="sidebar_subitem">
-                  <p className="sidebar_subitem_text left_align_stretch">{item.name}</p>
-                  <p className="sidebar_subitem_text right_align_stretch">{item.value}</p>
-                </div>
-              //<li key={item.name}>{item.name}: {item.value} <i>{item.lastupdate}</i></li>
+            {data.map((item, index) => (
+              <div  key={item.name}
+                    className="sidebar_subitem"
+                    style={index + 1 === data.length ? {border: 0} : {}}>
+                <span>{item.name}</span>
+                <span className="right">{item.value}</span>
+              </div>
             ))}
           </div>
         </div>
