157e92029be181a077f97f93ce758ab8310ecdba
Andreas Mårtensson <addem1234@gmail.com>
2016-04-27 12:58:52 +0200

    Sidebar improvements

--- js/sidebar.jsx
+++ js/sidebar.jsx
@@ -19,10 +19,6 @@ class Sidebar extends React.Component {
     this.getHistory  = this.getHistory.bind(this)
   }
 
-  componentDidMount() {
-    this.getHistory(moment().subtract(1, 'day'))
-  }
-
   componentWillReceiveProps(props) {
     if (props.roomId &&
         this.props.roomId === props.roomId &&
@@ -31,19 +27,21 @@ class Sidebar extends React.Component {
     }
     else if(this.props.roomId !== props.roomId) {
       this.setState({visible: true})
-      this.getHistory(props.roomId)
+      this.getHistory(moment().subtract(1, 'day'))
     }
   }
 
+  getHistoryCallback(startDate) {
+    return () => this.getHistory(startDate)
+  }
+
   getHistory(startDate) {
-    return event => {
-      this.setState({loaded: false})
-      var roomId = this.props.roomId
-      var type = this.props.mapContext
-      api.getHistory(roomId, type, startDate.toISOString()).then(json => {
-        this.setState({history: json, loaded: true})
-      })
-    }
+    this.setState({loaded: false})
+    var roomId = this.props.roomId
+    var type = this.props.mapContext
+    api.getHistory(roomId, type, startDate.toISOString()).then(json => {
+      this.setState({history: json, loaded: true})
+    })
   }
 
   handleClose() {
@@ -56,8 +54,7 @@ class Sidebar extends React.Component {
 
     var chartData = loaded ? {labels: history.labels, datasets:[{data: history.data}]} : false
     
-    var animation = visible ? 'animate-in' : 'animate-out'
-    
+    var animation = visible ? 'animate-in' : 'animate-out'    
     return (
       <div id='sidebar' className={'animate ' + animation}>
         <span onClick={this.handleClose} 
@@ -65,39 +62,44 @@ class Sidebar extends React.Component {
 
         <h1>{name}</h1>
 
-        <div className='item'>
-          <div className='item-head'>
-            <h2>Current Data</h2>
-          </div>
-          <div className='item-list'>
-            {
-              data.map(item => (
-                <div key={item.type}>
-                  <span>{item.type}</span>
-                  <span className='data' title={item.collected}>
-                    {Math.round(item.value * 10)/10}
-                  </span>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-        <div className='item'>
-          <div className='item-head'>
-            <h2>Historical Data</h2>
-          </div>
+        {data.length === 0 ? 
+          (<div className='missing'>There are no sensors in this room</div>) : 
+          (<div>
+            <div className='item'>
+              <div className='item-head'>
+                <h2>Current Data</h2>
+              </div>
+              <div className='item-list'>
+                {
+                  data.map(item => (
+                    <div key={item.type}>
+                      <span>{item.type}</span>
+                      <span className='data' title={item.collected}>
+                        {Math.round(item.value * 10)/10}
+                      </span>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+            <div className='item'>
+              <div className='item-head'>
+                <h2>Historical Data</h2>
+              </div>
 
-          <div className='item-range-select'>
-            <a onClick={this.getHistory(moment().subtract(1, 'day'))}>1 Day</a>
-            <a onClick={this.getHistory(moment().subtract(3, 'day'))}>3 Day</a>
-            <a onClick={this.getHistory(moment().subtract(1, 'week'))}>1 Week</a>
-            <a onClick={this.getHistory(moment().subtract(1, 'month'))}>1 Month</a>
-          </div>
+              <div className='item-range-select'>
+                <a onClick={this.getHistoryCallback(moment().subtract(1, 'day'))}>1 Day</a>
+                <a onClick={this.getHistoryCallback(moment().subtract(3, 'day'))}>3 Day</a>
+                <a onClick={this.getHistoryCallback(moment().subtract(1, 'week'))}>1 Week</a>
+                <a onClick={this.getHistoryCallback(moment().subtract(1, 'month'))}>1 Month</a>
+              </div>
 
-          <Loader loaded={loaded}>
-            <LineChart data={chartData} />
-          </Loader>
-        </div>
+              <Loader loaded={loaded}>
+                <LineChart data={chartData} />
+              </Loader>
+            </div>
+          </div>
+        )}
       </div>
     )
   }
